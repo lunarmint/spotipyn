d85a38007387edf3ff5f4ef75c042be2d118830b
Mint <developer@chocomint.dev>
2021-11-19 06:24:54 -0500

    Code lint and remove unused variables

--- modules/playlist/playlist.py
+++ modules/playlist/playlist.py
@@ -1,28 +1,23 @@
 import spotipy
 from flask import Blueprint, render_template
+
 from utils.auth import get_auth_manager
 
-playlist_blueprint = Blueprint('playlist_blueprint', __name__,
-                               template_folder="templates",
-                               static_folder="static",
-                               url_prefix="/playlist",
-                               )
+playlist_blueprint = Blueprint(
+    "playlist_blueprint",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    url_prefix="/playlist",
+)
 
-@playlist_blueprint.route('/')
+
+@playlist_blueprint.route("/")
 def playlist():
     # Fetch the OAuth2 object.
     auth_manager = get_auth_manager()
 
-    # Initialize the Spotify object to do stuffs.
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-
     # Get the access token value from the token dict in OAuth2 object.
     access_token = auth_manager.get_access_token()["access_token"]
 
-    # Get the current user info dict.
-    spotify_user = spotify.current_user()
-
-
-
-    return render_template("playlist.html", spotify=spotify, access_token=access_token)
-
+    return render_template("playlist.html", access_token=access_token)
