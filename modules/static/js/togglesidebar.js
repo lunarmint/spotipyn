e943dc25ab07891356df86743bee8955e772ce65
MageFurst <33272522+MageFurst@users.noreply.github.com>
2021-12-02 19:51:46 -0500

    New Sidebar
    Added new sidebar along with vh/vw instead of px.

--- modules/static/js/togglesidebar.js
+++ modules/static/js/togglesidebar.js
@@ -1,3 +1,14 @@
-function toggleSidebar(ref){
-        document.getElementById("sidebar").classList.toggle('active');
+var mini = true;
+function toggleSidebar() {
+if (mini) {
+console.log(“opening sidebar”);
+document.getElementById(“mySidebar”).style.width = “250px”;
+document.getElementById(“main”).style.marginLeft = “250px”;
+this.mini = false;
+} else {
+console.log(“closing sidebar”);
+document.getElementById(“mySidebar”).style.width = “100px”;
+document.getElementById(“main”).style.marginLeft = “100px”;
+this.mini = true;
+ }
 }
