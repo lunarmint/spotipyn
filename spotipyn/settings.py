f31ff2653bc38a01cbf09dd995ea9bbec02df993
lunarmint <developer@chocomint.dev>
2023-02-13 10:37:52 -0600

    Move secret key to config

--- spotipyn/settings.py
+++ spotipyn/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8c597(@&f!w8!)5wff63-^ph0^jum7@vco8t%_y-!tpc()if65'
+SECRET_KEY = config["spotipyn"]["django_secret_key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config["spotipyn"]["debug"]
