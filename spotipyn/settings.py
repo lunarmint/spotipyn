e47eac85bbe4baa2ce97545d51375c125e5656f2
lunarmint <developer@chocomint.dev>
2023-02-13 08:12:28 -0600

    Update settings.py

--- spotipyn/settings.py
+++ spotipyn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from spotipyn.utils.config import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8c597(@&f!w8!)5wff63-^ph0^jum7@vco8t%_y-!tpc()if65'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config["spotipyn"]["debug"]
 
 ALLOWED_HOSTS = []
 
